df75594e47330c945a69cfd903e221ec8e8b1eef
gavinDCHS <carroll_gavin@443mail.org>
2024-09-05 09:11:16 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let usedNumbers = [];
 let numOfStudents;
-const students = {1: ['David','Lucas', 'Aiden', 'Abigail', 'Jordan', 'Isaac', 'Candelaria', 'Alexis', 'Juana C.', 'Alexander', 'Samuel', 'Amir', 'Dellis', 'Ericka', 'Alondra', 'Frankie', 'Joselyn', 'Miguel', 'Victor', 'Sylas', 'Gabriella', 'Juana C.', 'Juana S.', 'Kelly', 'Austin', 'Marisola', 'Daniel', 'Litzy', 'Jazmani'], 4: ['Hannah', 'Cooper', 'Matea', 'Alton', 'Eimy', 'Pedro', 'Lesly', 'Marlene', 'Katelyn', 'Perla', 'Anndy', 'Andrea', 'Alton', 'Juan'], 5: ['Marcos', 'Jazlyn', 'Michelle', 'Oscar', 'Jayden', 'Jaylin', 'Alexa', 'Paloma', 'Andi', 'Kevin', 'Tomasa', 'Esmeralda', 'Adrian', 'Cristian', 'Oscar', 'Jayden', 'Marwa', 'Wilber', 'Jose', 'Erica', 'Kevin', 'David', 'Johan']};
+const students = {1: ['Alexa', 'Mariana', 'Angel', 'Juana', 'Juan', 'Ernesto', 'Payton', 'Katrina'], 4: ['Alex', 'Luis', 'Aleena', 'Abigail', 'Diego', 'Yairo', 'Alexa', 'Alonso', 'Candelaria', 'Zabian', 'Xavier', 'Esmeralada', 'Fabian', 'Jared', 'Vianca', 'Jocelyn', 'Jaselyn', 'Mariana', 'Jaylin'], 5: ['Javier', 'Cristiano', 'Jose', 'Ryan', 'Janie', 'Oscar', 'Kaeden', 'Yeraldin', 'Daivendz', 'Yareli', 'Jonathan', 'Edhin', 'Jackie', 'Keren', 'Sebastian', 'Hayden', 'Melisssa', 'Sammy']};
 let calledStudents = [];
 
 const animateCSS = (selector, animation, prefix = 'animate__') =>
